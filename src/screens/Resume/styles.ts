fa9db0f524b9237e9de1619c963a34cf1760862b
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-02-16 23:07:25 -0300

    feat: total by category listed and displayed in summary

--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -17,8 +17,13 @@ export const Header = styled.View `
   padding-bottom: 19px;
 `
 
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: { flex: 1, padding: 24}
+}) ``
+
 export const Title = styled.Text `
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(18)}px;
   color: ${({ theme }) => theme.colors.shape}
 `
+
